6054e0fd02f6195c7db927830ef90d42ca145752
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-18 08:17:49 +0000

    [update]19

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_params, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+  
+  def about_sign_out_path_for(resource)
+    about_path
+  end
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
